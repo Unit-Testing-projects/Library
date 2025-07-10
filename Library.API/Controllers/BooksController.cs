f9772e2d208803829b1c81df2313bfac57c4324f
Hayotbahrom Nurdinov <khayotbakhrom_nurdinov@epam.com>
2025-07-10 19:23:35 +0500

    Implemented BooksController

---
+++ Library.API/Controllers/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using Library.API.Data.Models;
+using Library.API.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Library.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _service;
+
+        public BooksController(IBookService bookService)
+        {
+            _service = bookService;
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Book> Get(int id)
+        {
+            var item = _service.GetById(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpGet]
+        // GET: api/<BooksController>
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+            var items = _service.GetAll();
+            return Ok(items);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Book value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var item = _service.Add(value);
+            return CreatedAtAction("Get", new { id = item.Id, item });
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var existingItem = _service.GetById(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            _service.Remove(id);
+            return Ok();
+        }
+    }
+}
--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -1,4 +1,6 @@
 
+using Library.API.Data.Services;
+
 namespace Library.API
 {
     public class Program
@@ -13,6 +15,8 @@ namespace Library.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IBookService, BookService>();
 
             var app = builder.Build();
 
